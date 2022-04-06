73a212771901e38833310ac68544111f4bf25ad1
emojapaula <vulpaula@gmail.com>
2022-04-06 22:01:27 +0200

    Fix homework1

--- MovieApp/MovieDetailsViewController.swift
+++ MovieApp/MovieDetailsViewController.swift
@@ -10,282 +10,279 @@ import SnapKit
 
 class MovieDetailsViewController: UIViewController, UIScrollViewDelegate{
     
+    
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
-    private let scrollView = UIScrollView()
-    let screenSize: CGRect = UIScreen.main.bounds
+    private var scrollView: UIScrollView!
+    private var imageView: UIImageView!
+    private var movieUserScoreValue: UILabel!
+    private var movieUserScoreValuePercentage: UILabel!
+    private var movieUserScoreLabel: UILabel!
+    private var movieTitle: UILabel!
+    private var movieYear: UILabel!
+    private var movieDateAndCountry: UILabel!
+    private var movieGenre: UILabel!
+    private var movieDuration: UILabel!
+    private var favoritesIcon: UIImage!
+    private var favoritesButton: UIButton!
+    private var detailsView: UIView!
+    private var movieOverviewLabel: UILabel!
+    private var movieOverview: UILabel!
+    private var crewView: UIView!
     
-    
-    func hexStringToUIColor (hex:String) -> UIColor {
-        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-
-        if (cString.hasPrefix("#")) {
-            cString.remove(at: cString.startIndex)
-        }
-
-        if ((cString.count) != 6) {
-            return UIColor.gray
-        }
-
-        var rgbValue:UInt64 = 0
-        Scanner(string: cString).scanHexInt64(&rgbValue)
-
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
-    }
-    
-    
-    private lazy var ImageView : UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "witcherThumbnail"))
+    let movieCrew = [
+                (name: "Lauren Schmidt", role: "Creator", nameComponent: UILabel(), roleComponent: UILabel()),
+                (name: "Andrzej Sapkowski", role: "Author", nameComponent: UILabel(), roleComponent: UILabel()),
+                (name: "Alik Sakharov", role: "Director", nameComponent: UILabel(), roleComponent: UILabel()),
+                (name: "Mike Ostrowski", role: "Producer", nameComponent: UILabel(), roleComponent: UILabel()),
+                (name: "Jean-Philippe Gossart", role: "Cinematographer", nameComponent: UILabel(), roleComponent: UILabel()),
+                (name: "Gavin Struthers", role: "Cinematographer", nameComponent: UILabel(), roleComponent: UILabel())
+            ]
+           
+    func styleViews() {
+        
         imageView.contentMode = .scaleAspectFit
         imageView.clipsToBounds = true
-        return imageView
-    }()
-    
-    
-    private lazy var movieUserScoreValue : UILabel = {
-        let label = UILabel()
-        label.text = "76"
-        label.textColor = .white
-        label.font = UIFont(name: "ProximaNova-Bold", size: 15)
-        return label
-    }()
-    
-    private lazy var movieUserScoreValuePercentage : UILabel = {
-        let label = UILabel()
-        label.text = "%"
-        label.textColor = .white
-        label.font = UIFont(name: "ProximaNova-Bold", size: 9)
-        return label
-    }()
-    
-    private lazy var movieUserScoreLabel : UILabel = {
-        let label = UILabel()
-        label.text = "User Score"
-        label.textColor = .white;
-        label.font = UIFont(name: "ProximaNova-Bold", size: 14)
-        return label
-    }()
-    
-    private lazy var movieTitle : UILabel = {
-        let label = UILabel()
-        label.text = "Witcher"
-        label.textColor = .white;
-        label.font = UIFont.systemFont(ofSize: 24, weight: .black)
-        label.font = UIFont(name: "ProximaNova-Extrabld", size: 24)
-        return label
-    }()
-    
-    private lazy var movieYear : UILabel = {
-        let label = UILabel()
-        label.text = "(2019)"
-        label.textColor = .white;
-        label.font = UIFont(name: "ProximaNova-Regular", size: 24)
-        return label
-    }()
-    
-    private lazy var movieDateAndCountry : UILabel = {
-        let label = UILabel()
-        label.text = "12/20/2019 (US)"
-        label.textColor = .white;
-        label.font = UIFont(name: "ProximaNova-Regular", size: 14)
-        return label
-    }()
-
-    private lazy var movieGenre : UILabel = {
-        let label = UILabel()
-        label.text = "Action, Adventure, Drama, Fantasy"
-        label.textColor = .white;
-        label.font = UIFont(name: "ProximaNova-Regular", size: 14)
-        return label
-    }()
-
-    private lazy var movieDuration : UILabel = {
-        let label = UILabel()
-        label.text = "2 seasons"
-        label.textColor = .white;
-        label.font = UIFont(name: "ProximaNova-Bold", size: 14)
-        return label
-    }()
+        
+        movieUserScoreValue.text = "84"
+        movieUserScoreValue.textColor = .white
+        movieUserScoreValue.font = UIFont(name: "ProximaNova-Bold", size: 15)
+        
+        movieUserScoreValuePercentage.text = "%"
+        movieUserScoreValuePercentage.textColor = .white
+        movieUserScoreValuePercentage.font = UIFont(name: "ProximaNova-Bold", size: 9)
+        
+        movieUserScoreLabel.text = "User Score"
+        movieUserScoreLabel.textColor = .white;
+        movieUserScoreLabel.font = UIFont(name: "ProximaNova-Bold", size: 14)
+        
+        movieTitle.text = "Witcher"
+        movieTitle.textColor = .white;
+        movieTitle.font = UIFont(name: "ProximaNova-Extrabld", size: 24)
+        
+        movieYear.text = "(2019)"
+        movieYear.textColor = .white;
+        movieYear.font = UIFont(name: "ProximaNova-Regular", size: 24)
+        
+        movieDateAndCountry.text = "12/20/2019 (US)"
+        movieDateAndCountry.textColor = .white;
+        movieDateAndCountry.font = UIFont(name: "ProximaNova-Regular", size: 14)
+        
+        movieGenre.text = "Action, Adventure, Drama, Fantasy"
+        movieGenre.textColor = .white;
+        movieGenre.font = UIFont(name: "ProximaNova-Regular", size: 14)
+        
+        movieDuration.text = "2 seasons"
+        movieDuration.textColor = .white;
+        movieDuration.font = UIFont(name: "ProximaNova-Bold", size: 14)
+        
+        favoritesButton.layer.cornerRadius = 20
+        favoritesButton.backgroundColor = UIColor.darkGray
+        favoritesButton.setImage(favoritesIcon, for: .normal)
+        favoritesButton.tintColor = .white
+        favoritesButton.addTarget(self, action: #selector(self.addToFavorites), for: .touchUpInside)
+        
+        movieOverviewLabel.text = "Overview"
+        movieOverviewLabel.textColor = .black;
+        movieOverviewLabel.font = UIFont(name: "ProximaNova-Extrabld", size: 20)
+        
+        movieOverview.text = "Geralt of Rivia, a solitary monster hunter, struggles to find his place in a world where people often prove more wicked than beasts."
+        movieOverview.textColor = .black;
+        movieOverview.numberOfLines = 3
+        movieOverview.font = UIFont(name: "ProximaNova-Regular", size: 14.0)
+    }
     
     @objc func addToFavorites(e: AnyObject) {
-            //handle adding to favorites
-        }
-
-    
-    let favoritesIcon = UIImage(systemName: "star.fill")
-
-    private lazy var favoritesButton: UIButton = {
-        let circle = UIButton()
-        circle.layer.cornerRadius = 20
-        circle.backgroundColor = UIColor.darkGray
-        circle.setImage(favoritesIcon, for: .normal)
-        circle.tintColor = .white
-        circle.addTarget(self, action: #selector(self.addToFavorites), for: .touchUpInside)
-        return circle
-    }()
-
-    private lazy var DetailsView: UIView = {
-        let view = UIView()
-        return view
-    }()
-    
-    private lazy var movieOverviewLabel : UILabel = {
-        let label = UILabel()
-        label.text = "Overview"
-        label.textColor = .black;
-        label.font = UIFont(name: "ProximaNova-Extrabld", size: 20)
-        return label
-    }()
-    
-    private lazy var movieOverview : UILabel = {
-        let label = UILabel()
-        label.text = "Geralt of Rivia, a solitary monster hunter, struggles to find his place in a world where people often prove more wicked than beasts."
-        label.textColor = .black;
-        label.numberOfLines = 3
-        let font = UIFont(name: "ProximaNova-Regular", size: 14.0)
-        label.font = font
-        return label
-    }()
-    
+        //handle adding to favorites
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        createViews()
+        styleViews()
         scrollView.contentInsetAdjustmentBehavior = .never
         scrollView.delegate = self
+        renderCrew()
         layout()
+    }
+    
+    func createViews() {
+        scrollView = UIScrollView()
+        view.addSubview(scrollView)
+        
+        imageView = UIImageView(image: UIImage(named: "witcherThumbnail"))
+        scrollView.addSubview(imageView)
+        
+        movieUserScoreValue = UILabel()
+        movieUserScoreValuePercentage = UILabel()
+        movieUserScoreLabel = UILabel()
+        imageView.addSubview(movieUserScoreValue)
+        imageView.addSubview(movieUserScoreValuePercentage)
+        imageView.addSubview(movieUserScoreLabel)
+        
+        movieTitle = UILabel()
+        movieYear = UILabel()
+        imageView.addSubview(movieTitle)
+        imageView.addSubview(movieYear)
+        
+        movieDateAndCountry = UILabel()
+        imageView.addSubview(movieDateAndCountry)
+        
+        
+        movieGenre = UILabel()
+        movieDuration = UILabel()
+        imageView.addSubview(movieGenre)
+        imageView.addSubview(movieDuration)
+        
+        favoritesIcon = UIImage(systemName: "star")
+        favoritesButton = UIButton()
+        imageView.addSubview(favoritesButton)
+        
+        detailsView = UIView()
+        scrollView.addSubview(detailsView)
+        
+        movieOverviewLabel = UILabel()
+        movieOverview = UILabel()
+        detailsView.addSubview(movieOverviewLabel)
+        detailsView.addSubview(movieOverview)
+        
+        crewView = UIView()
+       scrollView.addSubview(crewView)
 
     }
     
-    private func layout() {
-        
-        view.addSubview(scrollView)
+    private func renderCrew() {
 
-        scrollView.snp.makeConstraints{(maker) in
-            maker.edges.equalTo(view)
+        for crewMember in movieCrew {
+            let name = crewMember.nameComponent
+            name.text = crewMember.name
+            name.textColor = .black
+            name.font = UIFont(name: "ProximaNova-Extrabld", size: 14)
+            let role = crewMember.roleComponent
+            role.text = crewMember.role
+            role.textColor = .black
+            role.font = UIFont(name: "ProximaNova-Regular", size: 14)
+            crewView.addSubview(name)
+            crewView.addSubview(role)
+        }
+    }
+    
+    
+    private func layout() { 
+        scrollView.snp.makeConstraints { maker in
+            maker.edges.equalTo(view.safeAreaLayoutGuide)
+            maker.bottom.equalTo(crewView.snp.bottom)
         }
 
-        scrollView.addSubview(ImageView)
-        
-        ImageView.snp.makeConstraints{ (maker) in
+        imageView.snp.makeConstraints{(maker) in
             maker.top.equalToSuperview()
-            maker.height.equalTo(screenSize.height / 2)
-            maker.width.equalTo(screenSize.width)
+            maker.height.equalTo(view).multipliedBy(0.5)
+            maker.width.equalToSuperview()
         }
-        
-        ImageView.addSubview(movieUserScoreValue)
-        ImageView.addSubview(movieUserScoreValuePercentage)
-        ImageView.addSubview(movieUserScoreLabel)
-        
+
         movieUserScoreValue.snp.makeConstraints{(maker) in
-            maker.left.equalToSuperview().offset(screenSize.width * 0.086)
-            maker.top.equalToSuperview().offset(ImageView.frame.height * 0.6)
+            maker.leading.equalToSuperview().offset(30)
+            maker.top.equalToSuperview().offset(imageView.frame.height * 0.6)
         }
         
         movieUserScoreValuePercentage.snp.makeConstraints{(maker) in
-            maker.left.equalTo(movieUserScoreValue.snp.right)
-            maker.bottom.equalTo(movieUserScoreValue)
+            maker.leading.equalTo(movieUserScoreValue.snp.trailing)
+            maker.bottom.equalTo(movieUserScoreValue).offset(-2)
         }
         
         movieUserScoreLabel.snp.makeConstraints{(maker) in
-            maker.left.equalTo(movieUserScoreValuePercentage.snp.right).offset(screenSize.width * 0.02)
+            maker.leading.equalTo(movieUserScoreValuePercentage.snp.trailing).offset(8)
             maker.top.equalTo(movieUserScoreValue)
         }
-        
-        ImageView.addSubview(movieTitle)
-        ImageView.addSubview(movieYear)
-        
+
         movieTitle.snp.makeConstraints{(maker) in
-            maker.left.equalToSuperview().offset(screenSize.width * 0.05)
-            maker.top.equalTo(movieUserScoreLabel).offset(ImageView.frame.height * 0.15)
+            maker.leading.equalToSuperview().offset(18)
+            maker.top.equalTo(movieUserScoreLabel).offset(imageView.frame.height * 0.15)
         }
         
         movieYear.snp.makeConstraints{(maker) in
             maker.top.equalTo(movieTitle)
-            maker.left.equalTo(movieTitle.snp.right).offset(screenSize.width * 0.02)
+            maker.leading.equalTo(movieTitle.snp.trailing).offset(8)
         }
-        
-        ImageView.addSubview(movieDateAndCountry)
-        
+
         movieDateAndCountry.snp.makeConstraints{(maker) in
-            maker.left.equalTo(movieTitle)
+            maker.leading.equalTo(movieTitle)
             maker.top.equalTo(movieTitle.snp.bottom).offset(3)
         }
-        
-        
-        ImageView.addSubview(movieGenre)
-        ImageView.addSubview(movieDuration)
-        
+
         movieGenre.snp.makeConstraints{(maker) in
-            maker.left.equalTo(movieTitle)
+            maker.leading.equalTo(movieTitle)
             maker.top.equalTo(movieDateAndCountry.snp.bottom).offset(3)
         }
         
         movieDuration.snp.makeConstraints{(maker) in
             maker.top.equalTo(movieGenre)
-            maker.left.equalTo(movieGenre.snp.right).offset(screenSize.width * 0.02)
+            maker.leading.equalTo(movieGenre.snp.trailing).offset(8)
         }
-        
-        ImageView.addSubview(favoritesButton)
         
         favoritesButton.snp.makeConstraints { (maker) in
             maker.height.equalTo(32)
             maker.width.equalTo(32)
-            maker.left.equalTo(movieTitle)
+            maker.leading.equalTo(movieTitle)
             maker.top.equalTo(movieGenre.snp.bottom).offset(10)
         }
         
-        //favoritesButton.addSubview(favoritesIcon)
-        
-        //icon.snp.makeConstraints { (maker) in
-        //    maker.width.height.equalToSuperview().multipliedBy(0.5)
-       //     maker.center.equalToSuperview()
-       // }
-        
-        scrollView.addSubview(DetailsView)
-        
-        DetailsView.snp.makeConstraints{(maker) in
-            maker.top.equalTo(ImageView.snp.bottom)
-            maker.left.equalTo(view)
-            maker.width.equalTo(screenSize.width)
-            maker.height.equalTo(screenSize.height/4)
+        detailsView.snp.makeConstraints{(maker) in
+            maker.top.equalTo(imageView.snp.bottom)
+            maker.leading.equalToSuperview()
+            maker.width.equalToSuperview()
+            maker.height.equalTo(view).multipliedBy(0.25)
+            //maker.bottom.equalTo(movieCrew[3].roleComponent.snp.bottom).offset(390)
         }
-        
-        DetailsView.addSubview(movieOverviewLabel)
-        DetailsView.addSubview(movieOverview)
-        
+    
         movieOverviewLabel.snp.makeConstraints{(maker) in
             maker.top.equalToSuperview()
-            maker.left.equalToSuperview().offset(screenSize.width * 0.05)
+            maker.leading.equalToSuperview().offset(18)
         }
         
         movieOverview.snp.makeConstraints{(maker) in
             maker.top.equalTo(movieOverviewLabel.snp.bottom).offset(10)
-            maker.left.equalTo(movieOverviewLabel)
-            maker.left.equalTo(movieOverviewLabel.snp.left)
-            maker.right.equalTo(DetailsView.snp.right).offset(screenSize.width * -0.05)
-
+            maker.leading.equalToSuperview().offset(16)
+            maker.trailing.equalToSuperview().offset(-27)
         }
         
+        crewView.snp.makeConstraints {
+            $0.top.equalTo(detailsView.snp.bottom)
+            $0.bottom.greaterThanOrEqualToSuperview()
+            $0.bottom.equalTo(movieCrew[3].roleComponent.snp.bottom).offset(390)
+            $0.width.equalToSuperview()
+        }
         
-   
+        movieCrew[0].nameComponent.snp.makeConstraints{
+            $0.top.equalTo(movieOverview.snp.bottom).offset(24)
+            $0.leading.equalTo(movieOverview.snp.leading)
+            $0.width.equalToSuperview().dividedBy(3).offset(-20)
+        }
         
-    }    /*
-    // MARK: - Navigation
+        movieCrew[3].nameComponent.snp.makeConstraints{
+            $0.top.equalTo(movieCrew[0].roleComponent.snp.bottom).offset(10)
+            $0.leading.equalTo(movieCrew[0].roleComponent.snp.leading)
+            $0.width.equalTo(view.snp.width).dividedBy(3).offset(-20)
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        for (index, person) in movieCrew.enumerated() {
+            person.roleComponent.snp.makeConstraints{
+                $0.top.equalTo(person.nameComponent.snp.bottom).offset(5)
+                $0.leading.equalTo(person.nameComponent.snp.leading)
+            }
+            if (index != 0 && index != 3) {
+                person.nameComponent.snp.makeConstraints{
+                    $0.top.equalTo(movieCrew[index-1].nameComponent.snp.top)
+                    $0.leading.equalTo(movieCrew[index-1].nameComponent.snp.trailing).offset(10)
+                    $0.width.equalTo(view.snp.width).dividedBy(3).offset(-20)
+                }
+            }
+        }
     }
-    */
 }
 
 
